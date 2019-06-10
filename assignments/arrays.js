b98373a5b7218f61507d7796e161a890835ebfb3
Lambda_School_Loaner_125 <lambda_school_loaner_125@Lambda-School-Loaner-125s-MacBook-Pro.local>
2019-06-10 13:12:36 -0700

    Challenge 1

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -54,6 +54,11 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 {"id":50,"car_make":"Lincoln","car_model":"Town Car","car_year":1999}];
 
 
+ for (let i = 0; i < arr.length; i++) {
+     inventory[i]; 
+ }
+
+
 // Example for loop:
 
 // arr = [1,2,3,4];
@@ -63,7 +68,12 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+if (inventory.id == 33) {
+    console.log(this.car_year);
+    console.log(this.car_make);
+    console.log(this.car_model)
+}
+
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
