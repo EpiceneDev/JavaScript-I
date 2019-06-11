eb1474c1804651721695fc5234923f4354d11aab
Lambda_School_Loaner_125 <lambda_school_loaner_125@Lambda-School-Loaner-125s-MacBook-Pro.local>
2019-06-10 19:21:58 -0700

    MVP met

--- assignments/objects.js
+++ assignments/objects.js
@@ -73,19 +73,19 @@ console.log(antonietta.gender)
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-function speak () { 
-
-  return "Hello, my name is " + $[name] + "!"
+kennan.speak = function() {
+  return "Hello, my name is Kennan!";
 }
-//console.log(kennan.speak());
+
+console.log(kennan.speak())
 
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-let product = function (num1, num2) {
-  return num1*num2;
+antonietta.multiplyNums = function (num1, num2) {
+  return num1 * num2;
 }
 
-//console.log(antonietta.multiplyNums(3,4));
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
